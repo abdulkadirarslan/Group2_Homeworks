92a8f9e62eff53e46d0013edf589fe5299968e4e
Abdülkadir <abdulkadirarslan19@gmail.com>
2022-11-18 15:44:19 +0300

    tamamlandı.

--- src/day11/Dortgen.java
+++ src/day11/Dortgen.java
@@ -4,35 +4,35 @@ import java.util.Scanner;
 
 public class Dortgen {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        System.out.print("Kare için kenar : ");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Karenin kenarı : ");
         int kenar = input.nextInt();
-        System.out.print("Dikdörtgen için Kısa keanr : ");
+        System.out.print("Dikdörtgenin Kısa kenarı : ");
         int kisaKenar = input.nextInt();
-        System.out.print("Dikdörtgen için Uzun kenar : ");
-        int uzunKenar= input.nextInt();
+        System.out.print("Dikdörtgenin Uzun kenarı : ");
+        int uzunKenar = input.nextInt();
 
-        int kareninAlani=alanHesapla(kenar,kenar);
-        int kareninCevresi=cevreHesapla(kenar,kenar);
+        int kareninAlani = alanHesapla(kenar, kenar);
+        int kareninCevresi = cevreHesapla(kenar, kenar);
 
-        int dikdortgeninAlani=alanHesapla(kisaKenar,uzunKenar);
-        int dikdortgeninCevresi=cevreHesapla(kisaKenar,uzunKenar);
+        int dikdortgeninAlani = alanHesapla(kisaKenar, uzunKenar);
+        int dikdortgeninCevresi = cevreHesapla(kisaKenar, uzunKenar);
 
-        int toplamAlan=kareninAlani+dikdortgeninAlani;
-        int toplamCevre=kareninCevresi+dikdortgeninCevresi;
+        int alanlarınToplami = kareninAlani + dikdortgeninAlani;
+        int cevrelerinToplami = kareninCevresi + dikdortgeninCevresi;
 
-        System.out.println("Toplam Alan = "+toplamAlan);
-        System.out.println("Toplam Çevre = "+toplamCevre);
+        System.out.println("Alanların Toplamı = " + alanlarınToplami);
+        System.out.println("Çevrelerin Toplamı = " + cevrelerinToplami);
 
 
     }
 
-    public static int alanHesapla(int k1,int k2){
-        return k1*k2;
+    public static int alanHesapla(int k1, int k2) {
+        return k1 * k2;
     }
 
-    public static int cevreHesapla(int k1,int k2){
-        return 2*(k1+k2);
+    public static int cevreHesapla(int k1, int k2) {
+        return 2 * (k1 + k2);
     }
 
 }
